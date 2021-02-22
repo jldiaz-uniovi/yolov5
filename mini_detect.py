85745ba8dea7cf8d42a4152f86ac0bb4552ab021
JL Diaz (ec2) <jldiaz@uniovi.es>
2021-02-22 10:13:52 +0000

    Creado modulo mini_detect y usado por server.py
    Ese módulo simplifica al mínimo lo necesario para la aplicación de un
    modelo sobre una imagen dada y para la obtención de resultados,
    ya sea en forma de imagen etiquetada o de diccionario python de metadatos.
    
    El servidor flask ofrece ahora un radiobutton para obtener el resultado
    como imagen etiquetada o como JSON.

---
+++ mini_detect.py
@@ -0,0 +1,124 @@
+import sys
+import time
+from pathlib import Path
+import random
+import torch
+import cv2
+
+# Implementación propia de lo mínimo necesario para hacer la detección, que en realidad se
+# reduce a un par de líneas: cargar el modelo, y aplicarlo sobre una imagen
+
+# Se proporcionan dos funciones para ello, la que carga el modelo se ocupa de bajarlo
+# de torch.hub si no estaba previamente descargado. La segunda (detect) usa ese modelo
+# sobre una imagen y retorna el objeto de resultados (al que añade un campo .time con el
+# tiempo que tomó la detección de objetos)
+#
+# Otras dos funciones usan el objeto de resultados para generar a partir de él una imagen
+# con cajas de colores, o bien un JSON con la información sobre esas cajas.
+
+def load_model(model_name):
+    """Load the model from torch.hub
+
+    model_name: one of "yolov5s", "yolov5m", "yolov5l", "yolov5x"
+    """
+
+    return torch.hub.load('ultralytics/yolov5', model_name, pretrained=True)
+
+
+def detect(imagename, model):
+    """Applies the model on the image and returns the object with the results"""
+    
+    t1 = time.time()
+    r= model(imagename)
+    t2 = time.time()
+    r.time = t2-t1
+    return r
+
+
+def plot_one_box(x, img, color=None, label=None, line_thickness=None):
+    """Adds one bounding box with optional label to the image"""
+
+    # Plots one bounding box on image img
+    tl = line_thickness or round(0.002 * (img.shape[0] + img.shape[1]) / 2) + 1  # line/font thickness
+    color = color or [random.randint(0, 255) for _ in range(3)]
+    c1, c2 = (int(x[0]), int(x[1])), (int(x[2]), int(x[3]))
+    cv2.rectangle(img, c1, c2, color, thickness=tl, lineType=cv2.LINE_AA)
+    if label:
+        tf = max(tl - 1, 1)  # font thickness
+        t_size = cv2.getTextSize(label, 0, fontScale=tl / 3, thickness=tf)[0]
+        c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+        cv2.rectangle(img, c1, c2, color, -1, cv2.LINE_AA)  # filled
+        cv2.putText(img, label, (c1[0], c1[1] - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+
+def label_image(p, results, save_dir):
+    """Takes the results of the detection and draws several bounding boxes 
+    with labels on the image.
+
+    p: 
+        relative path and filename of the source image
+    results: 
+        the object returned by detect
+    save_dir: 
+        name of the folder to save the labelled image
+    """
+
+    p = Path(p)
+    save_dir = Path(save_dir)
+    save_path = str(save_dir / p.name) 
+    colors = [[random.randint(0, 255) for _ in range(3)] for _ in results.names]
+
+    im0 = cv2.imread(str(p))
+    # im0 = results.imgs[0]
+    for *xyxy, conf, clase in reversed(results.xyxy[0]):
+        label = f"{results.names[int(clase)]} {conf:.2f}"
+        plot_one_box(
+            xyxy,
+            im0,
+            label=label,
+            color=colors[int(clase)],
+            line_thickness=1,
+        )
+
+    # Save results (image with detections)
+    cv2.imwrite(save_path, im0)
+
+
+def get_meta(results):
+    """Extracts relevant information from the results of the detection
+    and generates a Python dictionary with it.
+
+    results:
+        The result object returned by detect
+
+    returns:
+        Python dictionary
+    """
+
+    meta = {
+        "file": results.files[0],
+        "time": results.time,
+        "results": [],
+    }
+    for *xyxy, conf, clase in results.xyxy[0]:
+        label = results.names[int(clase)]
+        dic = {
+            "bounding_box": [int(x) for x in xyxy],
+            "confidence": conf.item(),
+            "label": label
+        }
+        meta["results"].append(dic)
+    return meta
+
+
+# Example of usage
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("Usage:")
+        print(f"{sys.argv[0]} filename")
+        quit()
+    filename = sys.argv[1]
+    model = load_model("yolov5s")
+    r= detect(filename, model)
+    # r.print()
+    # label_image(filename, r, ".")
+    print(get_meta(r))
--- server.py
+++ server.py
@@ -1,54 +1,101 @@
 import os
-from flask import Flask, flash, request, redirect, url_for
+from flask import Flask, request, redirect, url_for, flash, render_template_string
 from flask import send_from_directory
+from flask.json import jsonify
 from werkzeug.utils import secure_filename
+from mini_detect import label_image, load_model, detect, get_meta
 
-app = Flask(__name__)
-
-
+# Some constants
 UPLOAD_FOLDER = './server/uploads'
 DETECTIONS_FOLDER = './server/detections'
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
+
+# Create app
+app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DETECTIONS_FOLDER'] = DETECTIONS_FOLDER
+app.config['SECRET_KEY'] = "replace-me"
 
+# Prepare Yolo model
+model = load_model("yolov5m")
+
+# Utility functions
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+def detect_and_label_image(filename):
+    _in = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    _out = app.config['DETECTIONS_FOLDER']
+    r = detect(_in, model)
+    label_image(_in, r, _out)
+
+
+def detect_and_get_meta(filename):
+    _in = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    r = detect(_in, model)
+    return get_meta(r)
+
+# FLASK routes
 @app.route('/detect', methods=['GET', 'POST'])
 def upload_file():
-    if request.method == 'POST':
-        print(request.files)
-        if 'file' not in request.files:
-            print('Falta la parte del fichero')
-            return redirect(request.url)
-        file = request.files['file']
-        if file.filename == '':
-            print('No se ha seleccionado fichero')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            run_detect(filename)
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    if request.method == "GET":
+        return render_template_string('''
+        <!doctype html>
+        <head>
+        <title>YOLO5 - Subir imagen</title>
+        <style>
+        ul.flashes {
+            background-color: #f0d0d0;
+            color: #804040;
+            list-style-type: none;
+            width: 25%;
+        }
+        </style>
+        </head>
+        <body>
+        <h1>YOLO5 - Subir imagen</h1>
+        {% with messages = get_flashed_messages() %}
+        {% if messages %}
+            <ul class=flashes>
+            {% for message in messages %}
+            <li>{{ message }}</li>
+            {% endfor %}
+            </ul>
+        {% endif %}
+        {% endwith %}        
+        <form method=post enctype=multipart/form-data>
+            <input type="file" name="file" id="file" aria-label="File browser example">
+            <input type=submit value=Subir>
+            <p>Tipo de resultado</p>
+            <input type=radio id=imagen name=tipo value=imagen checked>
+            <label for=imagen>Imagen etiquetada</label>
+            <input type=radio id=json name=tipo value=json> 
+            <label for=json>JSON</label>
+        </form>
+        </body>
+        ''')
+    if 'file' not in request.files or not request.files["file"]:
+        flash('No se ha subido ningún fichero')
+        return redirect(request.url)
+    file = request.files['file']
+    if not file or not allowed_file(file.filename):
+        flash("Tipo de fichero no admitido (solo jpg o png)")
+        return redirect(request.url)
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    if request.form["tipo"] == "imagen":
+        detect_and_label_image(filename)
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+    else:
+        metadata = detect_and_get_meta(filename)
+        return jsonify(metadata)
+
+
 
 @app.route('/detections/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['DETECTIONS_FOLDER'],
                                filename)
-
-from my_detect import detect
-def run_detect(filename):
-    detect(os.path.join(app.config['UPLOAD_FOLDER'], filename))
