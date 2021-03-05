750f55b9e741da201b7f5c5f4b7fa2610cc48cee
JL Diaz (ec2) <jldiaz@uniovi.es>
2021-03-05 17:48:43 +0000

    Añadido script para lanzar gunicorn con numero adaptable de workers

---
+++ start-gunicorn.sh
@@ -0,0 +1,20 @@
+# This is the script which runs gunicorn with the appropriate number of workers
+# To run this script at startup using systemctl, write /etc/systemd/system/gunicorn.service
+# with the following contents:
+
+# [Unit]
+# After=network.target
+
+# [Service]
+# User=ubuntu
+# Group=www-data
+# WorkingDirectory=/home/ubuntu/yolov5
+# ExecStart=/bin/bash start-gunicorn.sh
+
+# [Install]
+# WantedBy=multi-user.target
+
+export NUM_CPUS=`nproc`
+export NUM_WORKERS=$((2*$NUM_CPUS +1))
+echo $NUM_WORKERS > /tmp/gunicorn_num_workers
+/home/ubuntu/venvs/yolo5/bin/gunicorn --workers $NUM_WORKERS --bind localhost:5000 server:app
